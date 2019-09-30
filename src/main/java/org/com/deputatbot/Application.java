2d059a490af8d00439b9b2fe505989a1abe79a91
Kollkaa <44326241+Kollkaa@users.noreply.github.com>
2019-09-30 15:55:36 +0300

    kolia

--- src/main/java/org/com/deputatbot/Application.java
+++ src/main/java/org/com/deputatbot/Application.java
@@ -68,7 +68,7 @@ public class Application {
         // JSoup Example 2 - Reading HTML page from URL
         Document doc;
         try {
-            doc = Jsoup.connect("https://dtputatbot.herokuapp.com/login").get();
+            doc = Jsoup.connect("https://deputatbot.herokuapp.com/login").get();
             title = doc.title();
         } catch (IOException e) {
             e.printStackTrace();
